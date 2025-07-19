d94d5116e94ee17685cb43e75f8f92cc47aa0ad4
Bots.Business <git.sync@bots.business>
2025-07-19 12:49:55 +0000

    Bots.Business export for bot id: 2051177

---
+++ commands/_start.js
@@ -0,0 +1,17 @@
+/*CMD
+  command: /start
+  help: 
+  need_reply: false
+  auto_retry_time: 
+  folder: 
+  answer: Привет! Это секретная возможность работы бота! 
+
+  <<KEYBOARD
+
+  KEYBOARD
+  aliases: r.e.p.o.
+  group: 
+CMD*/
+
+Привет! Ты попал на проверку!
+Ответь на несколько вопросов и получи награду! 
